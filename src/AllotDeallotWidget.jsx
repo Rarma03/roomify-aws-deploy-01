ef2f457954348a06702c1821abb10cd1e9665fe4
Rarma03 <raj0701verma@gmail.com>
2024-07-28 19:14:03 +0530

    update allotwidget

--- src/AllotDeallotWidget.jsx
+++ src/AllotDeallotWidget.jsx
@@ -89,7 +89,10 @@ export default function AllotDeallotWidget({ request }) {
                 ) : status === 2 ? (
                     <>
                         <div className="grid grid-cols-2 gap-4 mt-4">
-                            <button className="bg-green-600 hover:bg-green-400 text-white py-2 rounded-md cursor-not-allowed" disabled={true}>Place Allocated</button>
+                            <button className="bg-green-600 hover:bg-green-400 text-white py-2 rounded-md cursor-not-allowed flex justify-center" disabled={true}>Place Allocated<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+                            </svg>
+                            </button>
                             <button className="bg-red-600 hover:bg-red-400 text-white py-2 rounded-md" onClick={de_allocate}>Reject Allocation</button>
                         </div>
                     </>
