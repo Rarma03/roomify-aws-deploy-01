67386fd6cca6df4b22bd84588a02af106a021bd9
Rarma03 <raj0701verma@gmail.com>
2024-07-28 11:59:30 +0530

    update accountnav mobile view

--- src/AccountNav.jsx
+++ src/AccountNav.jsx
@@ -1,6 +1,4 @@
-import { useContext, useEffect } from "react";
-import { Link, Navigate, useParams, useNavigate, useLocation } from "react-router-dom";
-import axios from "axios";
+import { useLocation, Link } from "react-router-dom";
 
 export default function AccountNav() {
     const { pathname } = useLocation();
@@ -8,40 +6,39 @@ export default function AccountNav() {
     if (subpage === undefined) {
         subpage = 'profile';
     }
+
     function linkClasses(type = null) {
-        // const isActive = pathname === '/account' && type === 'profile';
-        let classes = 'rounded-full inline-flex gap-2 py-2 px-6';
+        let classes = 'rounded-full inline-flex gap-2 py-2 px-6 whitespace-nowrap flex items-center';
         if (type === subpage) {
             classes += ' bg-primary text-white';
-        }
-        else {
-            classes += 'bg-white';
+        } else {
+            classes += ' bg-gray-200 text-primary ';
         }
         return classes;
     }
 
     return (
-        <nav className="w-full flex mt-8 gap-4 justify-center  overflow-scroll sm:overflow-hidden">
+        <nav className="w-full flex mt-8 gap-1 md;gap-4 justify-center overflow-x-auto sm:overflow-visible text-xs pl-[340px] md:pl-0">
             <Link className={linkClasses('profile')} to={'/account'}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mr-1 ml-[40px] sm:ml-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-1 ml-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
                 </svg>
                 My Account
             </Link>
             <Link className={linkClasses('bookings')} to={'/account/bookings'}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mr-1 ml-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-1 ml-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
                 </svg>
                 My Bookings Status
             </Link>
             <Link className={linkClasses('rentingrequest')} to={'/account/rentingrequest'}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mr-1 ml-2">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 1 1-3 0m3 0a1.5 1.5 0 1 0-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m-9.75 0h9.75" />
                 </svg>
                 Manage Request
             </Link>
             <Link className={linkClasses('places')} to={'/account/places'}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 mr-1 ml-2">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 mr-1 ml-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 21v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21m0 0h4.5V3.545M12.75 21h7.5V10.75M2.25 21h1.5m18 0h-18M2.25 9l4.5-1.636M18.75 3l-1.5.545m0 6.205 3 1m1.5.5-1.5-.5M6.75 7.364V3h-3v18m3-13.636 10.5-3.819" />
                 </svg>
                 My Accommodation
